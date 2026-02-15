538f4e898a2785d6626ba330e3b96d74c9662e12
sambo0709 <sboye403@gmail.com>
2026-02-14 20:08:05 -0600

    countAliveSurvivors loops through survivors and checks isAlive

--- PrintReport.java
+++ PrintReport.java
@@ -1,20 +1,31 @@
-
- //Prints Release 1 summary output.
- //Shows total survivors, total zombies,
- //and how many survivors made it to safety.
- 
+// Prints Release 1 summary output.
+// Shows total survivors, total zombies,
+// and how many survivors made it to safety.
 public void printReport() {
 
     int totalSurvivors = survivors.size();
     int totalZombies = zombies.size();
     int survivorsAlive = countAliveSurvivors();
 
-    System.out.println("We have " + totalSurvivors + 
+    System.out.println("We have " + totalSurvivors +
             " survivors trying to make it to safety.");
 
-    System.out.println("But there are " + totalZombies + 
+    System.out.println("But there are " + totalZombies +
             " zombies waiting for them.");
 
-    System.out.println("It seems " + survivorsAlive + 
+    System.out.println("It seems " + survivorsAlive +
             " have made it to safety.");
+}
+
+private int countAliveSurvivors() {
+
+    int count = 0;
+
+    for (Survivor s : survivors) {
+        if (s.isAlive()) {
+            count++;
+        }
+    }
+
+    return count;
 }
