df258735346b097d7eae864714ec5b3ba7736460
sambo0709 <155212845+sambo0709@users.noreply.github.com>
2026-02-14 20:26:17 -0600

    Add files via upload

---
+++ CommonInfected.java
@@ -0,0 +1,10 @@
+public class CommonInfected extends Zombie {
+
+    private static final int START_HEALTH = 50;
+    private static final int START_ATTACK = 5;
+
+    public CommonInfected() {
+        super(START_HEALTH, START_ATTACK);
+    }
+}
+
