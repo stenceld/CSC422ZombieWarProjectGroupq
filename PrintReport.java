abfb0330fe0348cc7216dcf18f93df3883ed836b
sambo0709 <sboye403@gmail.com>
2026-02-14 20:01:59 -0600

    this is printreport for release 1.

---
+++ PrintReport.java
@@ -0,0 +1,20 @@
+
+ //Prints Release 1 summary output.
+ //Shows total survivors, total zombies,
+ //and how many survivors made it to safety.
+ 
+public void printReport() {
+
+    int totalSurvivors = survivors.size();
+    int totalZombies = zombies.size();
+    int survivorsAlive = countAliveSurvivors();
+
+    System.out.println("We have " + totalSurvivors + 
+            " survivors trying to make it to safety.");
+
+    System.out.println("But there are " + totalZombies + 
+            " zombies waiting for them.");
+
+    System.out.println("It seems " + survivorsAlive + 
+            " have made it to safety.");
+}
