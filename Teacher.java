f78eab330f48facf47c343ce1de07715f0c4150b
sambo0709 <155212845+sambo0709@users.noreply.github.com>
2026-02-16 01:45:42 -0600

    Delete Teacher.java

--- Teacher.java
+++
@@ -1,9 +0,0 @@
-public class Teacher extends Survivor {
-
-    private static final int START_HEALTH = 50;
-    private static final int START_ATTACK = 5;
-
-    public Teacher() {
-        super(START_HEALTH, START_ATTACK);
-    }
-}
